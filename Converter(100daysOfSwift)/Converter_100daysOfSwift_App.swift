d4df2f0c5f30ef3354b64e6691809b40f634893b
Андрій Пікусь <llityhga@gmail.com>
2024-06-18 12:06:25 +0300

    Initial Commit

---
+++ Converter(100daysOfSwift)/Converter_100daysOfSwift_App.swift
@@ -0,0 +1,17 @@
+//
+//  Converter_100daysOfSwift_App.swift
+//  Converter(100daysOfSwift)
+//
+//  Created by Wolf on 18.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Converter_100daysOfSwift_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
